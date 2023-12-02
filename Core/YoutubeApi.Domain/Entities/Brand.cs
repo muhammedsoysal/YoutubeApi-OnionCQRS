c9d47fab4be838b81ea8997c0c043d116985a690
Muhammed Soysal <Muhammed Soysal@LAPTOP-124H4QM2>
2023-12-02 23:30:05 +0300

    entityler oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities;
+
+public class Brand:EntityBase
+{
+    public Brand()
+    {
+        
+    }
+
+    public Brand(string name)
+    {
+        Name=name;
+    }
+    public required string Name{ get; set; }
+
+    
+}

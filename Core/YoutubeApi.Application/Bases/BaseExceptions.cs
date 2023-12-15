4c113fb1bc3db8e80314a563496854cd199e328c
Muhammed Soysal <Muhammed Soysal@LAPTOP-124H4QM2>
2023-12-15 15:11:46 +0300

    project rules tamamlandı

---
+++ Core/YoutubeApi.Application/Bases/BaseExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace YoutubeApi.Application.Bases;
+
+public class BaseExceptions:ApplicationException
+{
+	public BaseExceptions(){}
+	public BaseExceptions(string message):base(message) {}
+	
+	
+}
--- Core/YoutubeApi.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
+++ Core/YoutubeApi.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using YoutubeApi.Application.Features.Products.Rules;
 using YoutubeApi.Application.UnitOfWorks;
 using YoutubeApi.Domain.Entities;
 
@@ -7,14 +8,24 @@ namespace YoutubeApi.Application.Features.Products.Command.CreateProduct;
 public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, Unit>
 {
 	private readonly IUnitOfWork _unitOfWork;
+	private readonly ProductRules _productRules;
 
-	public CreateProductCommandHandler(IUnitOfWork unitOfWork)
+	public CreateProductCommandHandler(IUnitOfWork unitOfWork, ProductRules productRules)
 	{
 		_unitOfWork = unitOfWork;
+		_productRules = productRules;
 	}
 
 	public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
 	{
+		IList<Product> products = await _unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+
+		//if (products.Any(x => x.Title == request.Title))
+		//	throw new Exception("Aynı başlıkta ürün olamaz.");
+
+		await _productRules.ProductTitleMustNotBeSame(products, request.Title);
+		
 		Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
 
 		await _unitOfWork.GetWriteRepository<Product>().AddAsync(product);

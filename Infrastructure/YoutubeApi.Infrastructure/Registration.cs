678e401a1b06bf9e6537967f1e64680a59535390
Muhammed Soysal <Muhammed Soysal@LAPTOP-124H4QM2>
2023-12-16 13:05:59 +0300

    Tokebn service yapıldı, createtoken metodu yapılandırıldı

---
+++ Infrastructure/YoutubeApi.Infrastructure/Registration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using YoutubeApi.Infrastructure.Tokens;
+
+namespace YoutubeApi.Infrastructure
+{
+	public static class Registration
+	{
+		public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+		}
+	}
+}
